de8d16f7873b0ce840ac88d7e512cd0329c86d77
Tring-Kishore <kishore.c@tringapps.com>
2025-01-22 14:17:35 +0530

    Update-1 First.java

--- First.java
+++ First.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class First{
     public static void main(String[] args)
     {
-        System.out.print("Hello World");
+        System.out.print("Hello Tringapps");
     }
 }
