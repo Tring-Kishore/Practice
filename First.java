92ce0a34295d45c8388b7fca07332c9a29534ece
Tring-Kishore <kishore.c@tringapps.com>
2025-01-22 16:51:30 +0530

    final commit

--- First.java
+++ First.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class First{
     public static void main(String[] args)
     {
-        System.out.print("Hello world , welcome!!");
+        System.out.print("Hello world , welcome");
     }
 }
